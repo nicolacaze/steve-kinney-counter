bcf1fa9329af3901cb5f074385cd652d417f1ca0
Nicolas <nicolas.lacaze@gmail.com>
2021-10-05 16:47:53 +0200

    Create custom hook and add useRef

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import "./style.css";
 
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return Number(storage);
+    return initialState;
+  };
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value]);
+
+  return [value, setValue];
+};
+
 const Counter = ({ max, step }) => {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = useLocalStorage(0, "counter");
+  const countRef = useRef();
+
+  let message;
+  if (countRef.current < count) message = "Higher";
+  if (countRef.current > count) message = "Lower";
+  countRef.current = count;
 
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
 
+  useEffect(() => {
+    document.title = `Steve's counter is ${count}`;
+  }, [count]);
+
   return (
     <div className="counter">
+      <p>{message}</p>
       <div className="counter__scoring">
         <button className="counter__button" onClick={decrement}>
           Decrement
